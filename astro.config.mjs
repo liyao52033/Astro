ad58129b5fcbdd494630655fab507ed92ab4e6a9
liyao52033 <1503027549@qq.com>
2024-07-14 20:27:31 +0800

    feat(config): add site URL and RSS support, update layouts and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://astro-e88d55.netlify.app"
+});
